a013238fcf5ba871988f08f2a0bd61fa34b3573b
bonghs <bonghwangse@gmail.com>
2018-10-26 00:50:07 +0900

    2018-10-25 게시판 프래그먼트

--- app/src/main/java/me/synology/hsbong/patientphotostorage/list/BoardFragment.java
+++ app/src/main/java/me/synology/hsbong/patientphotostorage/list/BoardFragment.java
@@ -20,7 +20,6 @@ public class BoardFragment extends Fragment {
 
     // TODO: Customize parameter argument names
     private static final String ARG_COLUMN_COUNT = "column-count";
-    // TODO: Customize parameters
     private int mColumnCount = 1;
 
     /**
